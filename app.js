7ab0a76de861b922bdb6749cbc6c779b9b5e7085
Robin Jain <robinjain9587@gmail.com>
2019-05-29 16:55:43 +0530

    first commit

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-const yargs = require('yargs');
-
+const express = require("express");
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
 const geocode = require('./geocode/geocode');
 const darksky = require('./weather/darksky');
 
-// const geocode = require('../geocode/geocode');
-// const darksky = require('../weather/darksky');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('', express.static(path.join(__dirname + '')));
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
 
 
-const argv = yargs
-    .options({
-        a: {
-            demand : true,
-            alias : 'address',
-            describe : 'address for weather-app',
-            string : true
-        }
-    })
-    .help()
-    .alias('help' , 'h')
-    .argv;
-
-    
-
-geocode.geocodeAddress(argv.a , (error , results) => {
-    if(error)
-    {
-        console.log(error);
-    }
-    else{
-        console.log(JSON.stringify(results , undefined , 2));
-        darksky.darksky(results.latitude , results.longitude , (error , weatherresults) => {
-            if(error)
-            {
-                console.log(error);
-            }
-            else{
-                console.log(`temperature : ${weatherresults.temperature}`);
-                console.log(`apparent temperature : ${weatherresults.apparentTemperature}`);
-            }
-        });
-    }
+app.get('/', (req, res) => {
+    res.render("input.ejs");
 });
 
-//63afa5b6b7939736f4d5e3f91ff3cd5c
+app.post('/input', (req, res) => {
+    var input = req.body.location;
+    console.log(input);
+    geocode.geocodeAddress(input, (error, results) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log(JSON.stringify(results, undefined, 2));
+            darksky.darksky(results.latitude, results.longitude, (error, weatherresults) => {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    console.log(`temperature : ${weatherresults.temperature}`);
+                    console.log(`apparent temperature : ${weatherresults.apparentTemperature}`);
+                    res.redirect("/");
+                }
+            });
+        }
+    });
+});
+
+app.listen("3000" , (req,res) => {
+    console.log("server is started at 3000");
+});
+
